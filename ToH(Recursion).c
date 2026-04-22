ea2c23b7ec857a7462a18a32e56c4593a4d774d2
sai-rajeswari2007 <velagapudisairajeswari@gmail.com>
2026-04-22 19:07:26 +0530

    Implement Tower of Hanoi using recursion

---
+++ ToH(Recursion).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void tower(int n, char from, char to, char aux) {
+ if (n == 1) {
+ printf("Move disk 1 from %c to %c\n", from, to);
+ return;
+ }
+ tower(n-1, from, aux, to);
+ printf("Move disk %d from %c to %c\n", n, from, to);
+ tower(n-1, aux, to, from);
+}
+int main() {
+ int n;
+ printf("Enter number of disks: ");
+ scanf("%d", &n);
+ tower(n, 'A', 'C', 'B');
+ return 0;
+} 
